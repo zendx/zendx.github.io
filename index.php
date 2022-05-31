0d5dbb9387eac6ec81a7996ecc96c448a32bc5ad
zendx <donsheddy@live.co.uk>
2022-05-31 21:07:18 +0100

    New update

---
+++ index.php
@@ -0,0 +1,461 @@
+<!doctype html>
+
+<html lang="en" class="no-js">
+
+<head>
+	<title>ZendX | Lagos Web Development and Branding Firm</title>
+
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,700,600,300' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/flexslider.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/animate.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/YTPlayer.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen">
+	
+	<script src="http://www.youtube.com/player_api" id="YTAPI"></script>
+	
+</head>
+<body>
+
+	<!-- Container -->
+	<div id="container">
+		
+		<!-- home 
+			================================================== -->
+		<div class="section">
+			<div id="home-section">
+				
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							<div class="flexcaption">
+								<h1>Welcome to ZendX</h1>
+								<p>We design and develop awesome websites and smart applications, impactful identities using the latest industry standard technologies. </p>
+								<a href="#contact-section"><span>Learn More</span></a>
+							</div>
+						</li>
+						<li>
+							<div class="flexcaption">
+								<h1>We are <span>fast</span></h1>
+								<p>Our response and delivery timing is top notch </p>
+								<a href="#contact-section"><span>Get Started</span></a>
+							</div>
+						</li>
+						<li>
+							<div class="flexcaption">
+								<h1>We Deliver both locally and internationally</h1>
+								<p>Irrespective of where you're contacting us from, we ensure to deliver.</p>
+							</div>
+						</li>
+					</ul>
+			    </div>
+			</div>
+		</div>
+		<!-- End home -->
+
+		<!-- Header
+		    ================================================== -->
+		<header class="clearfix">
+			<!-- Static navbar -->
+			<div class="navbar navbar-default navbar-static-top">
+				<div class="innher-header">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#"><img alt="" src="images/logo.png"></a>
+					</div>
+					<div class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+
+							<li><a class="active" href="#home-section">Home</a></li>
+							<li><a href="#services-section">Services</a></li>
+							<li><a href="#about-section">About</a></li>
+							<li><a href="#contact-section">Contact Us</a></li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- End Header -->
+
+		<!-- services 
+			================================================== -->
+		<div class="section">
+			<div id="services-section">
+				<div class="container">
+					<div class="title-section triggerAnimation animated" data-animate="bounceIn">
+						<h1>Our Services</h1>
+						<p>Here's our safe space to let you know all we offer.</p>
+					</div>
+
+					<div class="services-box">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInLeft">
+									<a href="#"><i class="fa fa-css3"></i></a>
+									<div class="services-post-content">
+										<h3>Web Design</h3>
+										<p>We design and develop awesome websites and smart applications.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInUp">
+									<a href="#"><i class="fa fa-plane"></i></a>
+									<div class="services-post-content">
+										<h3>Web Development</h3>
+										<p>We have a strong development team to handle medium to large scale projects.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInRight">
+									<a href="#"><i class="fa fa-files-o"></i></a>
+									<div class="services-post-content">
+										<h3>Mobile App Development</h3>
+										<p>Wheather you seek to develop Android &amp; IOS Apps, We're here for you.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInLeft">
+									<a href="#"><i class="fa fa-thumbs-up"></i></a>
+									<div class="services-post-content">
+										<h3>Graphics Design</h3>
+										<p>Do you need Graphics for Web, Prints and Media. We got you covered.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInDown">
+									<a href="#"><i class="fa fa-bullhorn"></i></a>
+									<div class="services-post-content">
+										<h3>Voice-Over</h3>
+										<p>When it comes to handling your voice-over projects, we're your best plug.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInLeft">
+									<a href="#"><i class="fa fa-css3"></i></a>
+									<div class="services-post-content">
+										<h3>Consultation</h3>
+										<p>We give tech and business related consultation to our clients. </p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInLeft">
+									<a href="#"><i class="fa fa-css3"></i></a>
+									<div class="services-post-content">
+										<h3>Solar Installation</h3>
+										<p>Our qualified engineers are always on standby to attend to your installation.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInLeft">
+									<a href="#"><i class="fa fa-css3"></i></a>
+									<div class="services-post-content">
+										<h3>Smart Home</h3>
+										<p>DO you have this thought in mind but don't know how to go about it? Send Us a Quote</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="services-post triggerAnimation animated" data-animate="fadeInRight">
+									<a href="#"><i class="fa fa-docs"></i></a>
+									<div class="services-post-content">
+										<h3>ZendX Academy</h3>
+										<p>We teach Web Design, Graphics Design,Social Media Marketting etc</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="img-holder triggerAnimation animated" data-animate="bounceIn">
+						<img alt="" src="images/image.png">
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- End services -->
+
+		<!-- about 
+			================================================== -->
+		<div class="section">
+			<div id="about-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="biography triggerAnimation animated" data-animate="fadeInLeft">
+								<div class="about-title">
+									<h2>About US</h2>
+									<span>the Zendx Company</span>									
+								</div>
+								<p>We are a <span>Team of I.T Professionals</span> that specializes in different aspects of I.T. We have built lots of beautiful and <span>amazing projects</span> over the years which has made us stand-out in the industry.</p>
+
+								<p><span>ZendX</span> ensures that customer's satisfaction is always paramount therefore we ensure to bring your lovely ideas to <span>Life</span> in the stipulated time. We keep to our word.</p>
+
+								<p>We give expert consultation on how you can position your company / brand for success in this ever changing world as well as give you the right training for your company's success through the <span>ZendX Academy</span></p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="clients triggerAnimation animated" data-animate="fadeInRight">
+								<div class="about-title">
+									<h2>Our Clients</h2>
+									<span>We love our clients</span>									
+								</div>
+								<ul class="bxslider">
+									<li>
+										<ul class="clients-logo">
+											<li><a href="#"><img alt="" src="images/client-logo1.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo2.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo3.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo4.png"></a></li>
+										</ul>
+									</li>
+									<!--li>
+										<ul class="clients-logo">
+											<li><a href="#"><img alt="" src="images/client-logo1.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo2.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo3.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo4.png"></a></li>
+										</ul>
+									</li>
+									<li>
+										<ul class="clients-logo">
+											<li><a href="#"><img alt="" src="images/client-logo1.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo2.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo3.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo4.png"></a></li>
+										</ul>
+									</li>
+									<li>
+										<ul class="clients-logo">
+											<li><a href="#"><img alt="" src="images/client-logo1.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo2.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo3.png"></a></li>
+											<li><a href="#"><img alt="" src="images/client-logo4.png"></a></li>
+										</ul>
+									</li-->
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<!--div class="title-section triggerAnimation animated" data-animate="bounceIn">
+						<h1>Company stats</h1>
+						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida</p>
+					</div>
+
+					<div class="statistic-box triggerAnimation animated" data-animate="bounceIn">
+						<div class="row">
+							<div class="col-md-3">
+								<div class="statistic-post">
+									<span class="icon-stat"><i class="fa fa-flask"></i></span>
+									<p class="counter" data-perc="956779"><span class="numb"></span></p>
+									<p>Lines of code written</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="statistic-post">
+									<span class="icon-stat"><i class="fa fa-coffee"></i></span>
+									<p class="counter" data-perc="1479"><span class="numb"></span></p>
+									<p>Coffe Drinked</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="statistic-post">
+									<span class="icon-stat"><i class="fa fa-users"></i></span>
+									<p class="counter" data-perc="578"><span class="numb"></span></p>
+									<p>Happy Clients</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="statistic-post">
+									<span class="icon-stat"><i class="fa fa-briefcase"></i></span>
+									<p class="counter" data-perc="2178"><span class="numb"></span></p>
+									<p>Projects Done</p>
+								</div>
+							</div>
+						</div>
+					</div-->
+
+				</div>
+			</div>
+		</div>
+		<!-- End about -->
+
+		<!-- contact 
+			================================================== -->
+		<div class="section">
+			<div id="contact-section">
+				<div class="map"></div>
+
+				<div class="contact-content">
+					<div class="container">
+
+						<div class="title-section white triggerAnimation animated" data-animate="bounceIn">
+							<h1>Get in touch!</h1>
+							<p>We are always on ground to handle your requests.</p>
+						</div>
+
+						<div class="row">
+							<div class="col-md-6 triggerAnimation animated" data-animate="fadeInLeft">
+								<div class="contact-info">
+									<h1>Get A Quote</h1>
+									<p>We want to be a part of your company's success and growth by handling your project(s) for you</p>
+									<p>Well still need to have an in-depth understanding of your comapny's need so we can serve you better.</p>		
+									<p>Please use the form to tell us what you want us to do, and we'll respond with a quote and best hand on approach for your project.</p>						
+								</div>
+								<ul class="contact-info-list">
+									<li><i class="fa fa-home"></i> Plot 397 Oluti B/Stop Lagos-Badagry Expressway.</li>
+									<li><i class="fa fa-phone"></i> +234 803 197 5415</li>
+									<li><i class="fa fa-envelope"></i> <a href="#">zendx@gmail.com</a></li>
+								</ul>
+							</div>
+							<div class="col-md-6 triggerAnimation animated" data-animate="fadeInRight">
+								<form id="contact-form">
+									<div class="text-fields">
+										<div class="float-input">
+											<input name="name" id="name" type="text" placeholder="Full Name">
+											<span><i class="fa fa-user"></i></span>
+										</div>
+										<div class="float-input">
+											<input name="mail" id="mail" type="text" placeholder="E-mail">
+											<span><i class="fa fa-envelope-o"></i></span>
+										</div>
+										<div class="float-input">
+											<input name="phone" id="phone" type="text" placeholder="Phone">
+											<span><i class="fa fa-phone"></i></span>
+										</div>
+										<div class="float-select">
+											<select>
+												<option selected>Can we drop a whatsapp Message / Call?</option>
+												<option>Yes Please! :)</option>
+												<option>No please :(</option>
+											</select>
+										</div>
+									</div>
+
+									<div class="submit-area">
+										<div class="float-select">
+											<select>
+												<option>Consultation</option>
+												<option>Web Design</option>
+												<option>Web Development</option>
+												<option>Graphics Design</option>
+												<option>Voice-Over</option>
+												<option>Mobile App Development</option>
+												<option>Solar Installation</option>
+												<option>I want to join the ZendX academy</option>
+												<option selected>Where Do We Come In?</option>
+											</select>
+										</div>
+										<textarea name="comment" id="comment" placeholder="Tell us more about your idea.(You can add a link to your detailed brief here.)"></textarea>
+										<input type="submit" id="submit_contact" class="main-button" value="Send Message">
+										<div id="msg" class="message"></div>
+									</div>
+								</form>
+							</div>
+						</div>
+
+					</div>
+				</div>	
+
+			</div>
+		</div>
+		<!-- End contact -->
+
+
+		<!-- footer 
+			================================================== -->
+		<footer>
+			<div class="container">
+				<p>&#169; 2008 - <?php echo date('Y') ?> ZendX,  All Rights Reserved</p>
+				<ul class="social-icons">
+					<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+					<!--li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
+					<li><a class="google" href="#"><i class="fa fa-google-plus"></i></a></li>
+					<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+					<li><a class="pinterest" href="#"><i class="fa fa-pinterest"></i></a></li-->						
+				</ul>
+			</div>
+		</footer>
+		<!-- End footer -->
+	</div>
+	<!-- End Container -->
+
+	<div class="preloader">
+		<img alt="" src="images/preloader.html">
+	</div>
+	
+	<!-- Contents of first popup window -->
+	<div id="first-popup" class="mfp-hide white-popup">
+		<div class="some-element">
+			<div class="flexslider">
+			    <ul class="slides">
+					<li>
+						<img alt="" src="upload/single-project/1.jpg" />
+					</li>
+					<li>
+						<img alt="" src="upload/single-project/2.jpg" />
+					</li>
+					<li>
+						<img alt="" src="upload/single-project/3.jpg" />
+					</li>
+				</ul>
+			</div>
+			<h2>Useful Seo Icons</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolorei</p>
+		</div>
+		<p class="last-row">You can view the project <a href="#"> here</a></p>
+	</div>
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.migrate.js"></script>
+	<script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script>
+	<script type="text/javascript" src="js/jquery.appear.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/waypoints.js"></script>
+	<script type="text/javascript" src="js/jquery.imagesloaded.min.js"></script>
+  	<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+  	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7z3qSfW7_1ArWHGs69jHLbLw-jOOGwuk"></script>
+	<script type="text/javascript" src="js/gmap3.min.js"></script>
+	<script type="text/javascript" src="js/smooth-scroll.js"></script>
+	<script type="text/javascript" src="js/plugins-scroll.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+
+</html>
